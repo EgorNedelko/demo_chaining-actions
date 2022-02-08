effd5273c19e77581fcfa22f9f805152f40f885e
Egor Nedelko <gojiramedia@gmail.com>
2022-02-08 20:19:41 +0300

    milestone commit - continue work on onboarding

--- js/modals.js
+++ js/modals.js
@@ -114,7 +114,7 @@ document.addEventListener('DOMContentLoaded', () => {
          let targetElem = document.querySelector(".modal")
          let targetElemPos = targetElem.getBoundingClientRect()
          doggyHead.style.left = `${targetElemPos.x-195}px`
-         doggyHead.style.top = `${targetElemPos.y-140}px`
+         doggyHead.style.top = `${targetElemPos.y-136}px`
 
          //make doggy appear
          setTimeout(() => { doggyHead.querySelector('svg').setAttribute('opacity', '1') }, 1250)
@@ -152,7 +152,6 @@ document.addEventListener('click', (e) => {
             doggy.style.left = `${targetElemPos.x - targetElem.offsetWidth + 5}px`
             doggy.style.top = `${targetElemPos.y - targetElem.offsetHeight - 55}px`
 
-            
             //position Step1 Modal
             setTimeout(() => {
                targetElem.classList.add('onboarding-focus')
@@ -162,7 +161,7 @@ document.addEventListener('click', (e) => {
                document.querySelector('.modal').style.top = `${targetElemPos.y - targetElem.offsetHeight}px`
                document.querySelector('.modal').removeChild(document.querySelector('.modal-footer'))
             }, 600)
-            //Make doggy and modal visibe
+            //Make doggy and modal visible
             openTourModal()
             setTimeout(() => { doggy.querySelector('svg').setAttribute('opacity', '1') }, 1200)
          
@@ -206,11 +205,8 @@ window.addEventListener('keydown', (e) => {
       //Modal #2
       document.querySelector('.doggy svg').setAttribute('opacity', '0')
       document.querySelector('.container').append(buildModal(modalsHeaderContent["Step2"], modalsBodyContent["Step2"]))
-      let timeout = setTimeout(()=>{
-         overlay.classList.add('active')
-         document.querySelector('.modal').classList.add('active')
-         document.querySelector('.modal-content').classList.remove('invisible')
-      }, 1000)
+      document.querySelector('.modal').removeChild(document.querySelector('.modal-footer'))
+      openTourModal()
    }
 })
 
@@ -221,11 +217,27 @@ document.addEventListener('click', (e) => {
       //Modal #2
       document.querySelector('.doggy svg').setAttribute('opacity', '0')
       document.querySelector('.container').append(buildModal(modalsHeaderContent["Step2"], modalsBodyContent["Step2"]))
-      let timeout = setTimeout(()=>{
-         overlay.classList.add('active')
-         document.querySelector('.modal').classList.add('active')
-         document.querySelector('.modal-content').classList.remove('invisible')
-      }, 1000)
+
+      //position doggy on the target element
+      let doggy = document.querySelector('.doggy')
+      let targetElem = document.querySelector(".project-name")
+      let targetElemPos = targetElem.getBoundingClientRect()
+      doggy.style.left = `${targetElemPos.x - targetElem.offsetWidth + 110}px`
+      doggy.style.top = `${targetElemPos.y - targetElem.offsetHeight - 60}px`
+
+      //position Step2 Modal
+      setTimeout(() => {
+         targetElem.parentNode.classList.add('onboarding-focus')
+         let modal = document.querySelector('.modal')
+         document.querySelector('.container').append(buildModal(modalsHeaderContent["Step1"], modalsBodyContent["Step1"]))
+         document.querySelector('.modal').style.left = `${targetElemPos.x + document.querySelector('.modal').offsetWidth/2}px`
+         document.querySelector('.modal').style.top = `${targetElemPos.y + targetElem.offsetHeight*8}px`
+         document.querySelector('.modal').removeChild(document.querySelector('.modal-footer'))
+      }, 600)
+
+      //make doggy and modal visible
+      openTourModal()
+      setTimeout(() => { doggy.querySelector('svg').setAttribute('opacity', '1') }, 400)
    }
 })
 
